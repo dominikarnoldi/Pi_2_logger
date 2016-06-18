0cb8a892c60b19b18bfcbba611b2abc32d4f0b31
Dominik Arnoldi <dominik.arnoldi@econ-solutions.de>
2016-06-18 11:50:51 +0200

    starting developing on datalogger

--- logger.c
+++ logger.c
@@ -7,5 +7,12 @@
 
 int main(viod)
 {
-
+    wiringPiSetup();
+    pinMode (0, OUTPUT) ;
+    for (;;)
+    {
+        digitalWrite (0, HIGH) ; delay (500) ;
+        digitalWrite (0,  LOW) ; delay (500) ;
+    }
+    return 0 ;
 }
