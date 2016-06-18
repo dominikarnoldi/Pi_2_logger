fc7476010525f8003db27d7c9e0e1c34fc189ab2
Dominik Arnoldi <dominik.arnoldi@econ-solutions.de>
2016-06-18 11:34:32 +0200

    starting developing on datalogger

---
+++ logger.c
@@ -0,0 +1,11 @@
+//
+// Created by dominik.arnoldi on 18.06.2016.
+//
+
+#include "logger.h"
+#include <wiringPi.h>
+
+int main(viod)
+{
+
+}
---
+++ logger.h
@@ -0,0 +1,8 @@
+//
+// Created by dominik.arnoldi on 18.06.2016.
+//
+
+#ifndef PI_2_LOGGER_LOGGER_C_H
+#define PI_2_LOGGER_LOGGER_C_H
+
+#endif //PI_2_LOGGER_LOGGER_C_H
